e48ba925c8d16d0a8d05b0fc4226eaf36d39d40a
Alavi-Ullah <54385477+DoubleHelix3@users.noreply.github.com>
2021-03-12 13:53:14 -0600

    tried creating a build system

--- buildsystem/build.c
+++ buildsystem/build.c
@@ -3,6 +3,13 @@
 #include<string.h>
 
 int main() {
-    system("ls");
+    char *compile = " && gcc -c ../code/userinput.c";
+    char *link = " && dir"; 
+    // gcc -o ../code/main.c userinput.o
+    char *cmd = "cd ../build";
+    strcat(cmd, compile);
+    system(cmd);
+    strcat(cmd, link);
+    system(cmd);
     return 0;
 }
