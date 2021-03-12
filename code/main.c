d6768d8544dc38b578188617327289902190904b
Alavi-Ullah <54385477+DoubleHelix3@users.noreply.github.com>
2021-03-12 11:16:57 -0600

    created a file to store a build of the game

--- code/main.c
+++ code/main.c
@@ -1,6 +1,15 @@
 #include <stdio.h>
+#include <string.h>
+
+#define MAX_NAME_LENGTH 25
 
 int main() {
-    printf("Greetings Traveler!\n");
-    return 0;
+    printf("Greetings traveler!\n");
+    printf("What is your name?\n-");
+    char name[MAX_NAME_LENGTH];
+    fgets(name, MAX_NAME_LENGTH, stdin);
+    // get rid of that new line character at the end
+    name[strcspn(name, "\n")] = 0;
+    printf("%s is a nice name!\n", name);
+    return 0;l
 }
